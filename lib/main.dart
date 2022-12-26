47c9ddc2c941b8f85c0111413d20f835979d5514
ziad <ziad@yotech.ltd>
2022-12-26 19:22:01 +0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'FadeAnimation.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,23 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'Nunito'),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -32,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +29,153 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  dynamic _pageController;
+  int totalPage = 4;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: PageView(
+        controller: _pageController,
+        children: [
+          makeView(
+              page: 1,
+              image: 'assets/images/one.jpg',
+              title: 'Yosemite National Park',
+              description:
+                  'Yosemite National Park is in California’s Sierra Nevada mountains. It’s famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.'),
+          makeView(
+              page: 2,
+              image: 'assets/images/two.jpg',
+              title: 'Golden Gate Bridge',
+              description:
+                  'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.'),
+          makeView(
+              page: 3,
+              image: 'assets/images/three.jpg',
+              title: 'Sedona',
+              description:
+                  "Sedona is regularly described as one of America's most beautiful places. Nowhere else will you find a landscape as dramatically colorful."),
+          makeView(
+              page: 4,
+              image: 'assets/images/four.jpg',
+              title: 'Savannah',
+              description:
+                  "Savannah, with its Spanish moss, Southern accents and creepy graveyards, is a lot like Charleston, South Carolina. But this city about 100 miles to the south has an eccentric streak."),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+  Widget makeView({image, title, description, page}) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover
+          )
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                stops: [0.3, 0.9],
+                colors: [
+                  Colors.black.withOpacity(.9),
+                  Colors.black.withOpacity(.2),
+                ]
+            )
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Widget>[
+                    FadeAnimation(2, Text(page.toString(), style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                    Text('/' + totalPage.toString(), style: TextStyle(color: Colors.white, fontSize: 15),)
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FadeAnimation(1,
+                          Text(title, style: TextStyle(color: Colors.white, fontSize: 50, height: 1.2, fontWeight: FontWeight.bold),)
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FadeAnimation(1.5, Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(Icons.star, color: Colors.yellow, size: 15,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(Icons.star, color: Colors.yellow, size: 15,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(Icons.star, color: Colors.yellow, size: 15,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 3),
+                            child: Icon(Icons.star, color: Colors.yellow, size: 15,),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 5),
+                            child: Icon(Icons.star, color: Colors.grey, size: 15,),
+                          ),
+                          Text('4.0', style: TextStyle(color: Colors.white70),),
+                          Text('(2300)', style: TextStyle(color: Colors.white38, fontSize: 12),)
+                        ],
+                      )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FadeAnimation(2, Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Text(description, style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15),),
+                      )),
+                      SizedBox(height: 20,),
+                      FadeAnimation(2.5, Text('READ MORE', style: TextStyle(color: Colors.white),)),
+                      SizedBox(height: 30,),
+                    ],
+                  ),
+                )
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+
 }
