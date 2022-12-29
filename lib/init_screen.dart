1e4229999587a4332bc9cd35cb76516e60fc3088
ziad <ziad@yotech.ltd>
2022-12-29 15:16:35 +0600

    started

---
+++ lib/init_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InitScreen extends StatefulWidget {
+  const InitScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitScreen> createState() => _InitScreenState();
+}
+
+class _InitScreenState extends State<InitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
